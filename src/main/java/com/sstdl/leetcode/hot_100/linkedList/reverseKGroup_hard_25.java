b9cd45d65dbe512035cb3d0ee9747e9515bed12b
SSTDL <2010206234@qq.com>
2025-03-14 15:29:01 +0800

    AC*2 （链表） 2025.03.14

--- src/main/java/com/sstdl/leetcode/hot_100/linkedList/reverseKGroup_hard_25.java
+++ src/main/java/com/sstdl/leetcode/hot_100/linkedList/reverseKGroup_hard_25.java
@@ -1,7 +1,5 @@
 package com.sstdl.leetcode.hot_100.linkedList;
 
-import java.util.ArrayList;
-
 /**
  * @author SSTDL
  * @description 25. K 个一组翻转链表
@@ -29,38 +27,88 @@ public class reverseKGroup_hard_25 {
         }
     }
 
-    // 使用额外数组遍历-AC（手撕）
+    // 不使用额外数组-AC（看解）
+    // 查找子链表的尾节点，重新连接链表
     public static ListNode reverseKGroup(ListNode head, int k) {
-        ListNode curr = head;
-        ArrayList<Integer> list = new ArrayList<>();
-        while (curr != null) {
-            list.add(curr.val);
-            curr = curr.next;
-        }
-        int len = list.size();
-        int[] nums = new int[len];
-        for (int i = 0; i < len; i++) {
-            nums[i] = list.get(i);
-        }
-        ListNode newHead = new ListNode(nums[k - 1]);
-        ListNode newCurr = newHead;
-        int kVolume = len - len % k;
-        for (int i = k - 2; i >= 0; i --) {
-            newCurr.next = new ListNode(nums[i]);
-            newCurr = newCurr.next;
-        }
-        if (kVolume - k > 0) {
-            for (int i = 2 * k - 1; i < kVolume; i += k ) {
-                for (int j = i; j > i - k; j--) {
-                    newCurr.next = new ListNode(nums[j]);
-                    newCurr = newCurr.next;
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode prev = dummyHead;
+        while (head != null) {
+            ListNode tail = prev;
+            for (int i = 0; i < k; i++) {
+                // 找到当前子链表的尾节点
+                tail = tail.next;
+                if (tail == null) {
+                    return dummyHead.next;
                 }
             }
+            // 保存 tail 的下一个节点 nex，用于后续连接
+            ListNode next = tail.next;
+            // 翻转链表
+            ListNode[] listNodes = reverseListNode(head, tail);
+            head = listNodes[0];
+            tail = listNodes[1];
+            // 重新链接回原链表
+            prev.next = head;
+            tail.next = next;
+            // 更新 prev 为当前子链表的尾节点，准备处理下一个子链表
+            prev = tail;
+            // 更新 head 为下一个子链表的头节点
+            head = tail.next;
         }
-        for (int i = kVolume; i < len; i++) {
-            newCurr.next = new ListNode(nums[i]);
-            newCurr = newCurr.next;
-        }
-        return newHead;
+        return dummyHead.next;
     }
+
+    public static ListNode[] reverseListNode(ListNode head, ListNode tail) {
+        // 反转后 head 的 next 指针应该指向的节点
+        ListNode prev = tail.next;
+        ListNode curr = head;
+        while (prev != tail) {
+            // 保存当前节点的下一个节点
+            ListNode next = curr.next;
+            // 讲当前节点的下一个节点指向 prev，实现翻转
+            curr.next = prev;
+            // 更新 prev 为当前节点
+            prev = curr;
+            // 移动当前节点
+            curr = next;
+        }
+        return new ListNode[]{tail, head};
+    }
+
+
+    // 使用额外数组遍历-AC（手撕）
+//    public static ListNode reverseKGroup(ListNode head, int k) {
+//        ListNode curr = head;
+//        ArrayList<Integer> list = new ArrayList<>();
+//        while (curr != null) {
+//            list.add(curr.val);
+//            curr = curr.next;
+//        }
+//        int len = list.size();
+//        int[] nums = new int[len];
+//        for (int i = 0; i < len; i++) {
+//            nums[i] = list.get(i);
+//        }
+//        ListNode newHead = new ListNode(nums[k - 1]);
+//        ListNode newCurr = newHead;
+//        int kVolume = len - len % k;
+//        for (int i = k - 2; i >= 0; i --) {
+//            newCurr.next = new ListNode(nums[i]);
+//            newCurr = newCurr.next;
+//        }
+//        if (kVolume - k > 0) {
+//            for (int i = 2 * k - 1; i < kVolume; i += k ) {
+//                for (int j = i; j > i - k; j--) {
+//                    newCurr.next = new ListNode(nums[j]);
+//                    newCurr = newCurr.next;
+//                }
+//            }
+//        }
+//        for (int i = kVolume; i < len; i++) {
+//            newCurr.next = new ListNode(nums[i]);
+//            newCurr = newCurr.next;
+//        }
+//        return newHead;
+//    }
 }
